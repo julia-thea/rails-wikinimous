57c4d1aa14c5c60bcf52dec996dc45da1425786d
Julia Thea <yuliya.boyadjan@gmail.com>
2020-02-25 14:29:35 -0600

    Added controller methods for create, edit, new, update, and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-  get 'index', to: 'articles#index'
-  get 'articles/:id', to: 'articles#show', as: :article
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
   post 'articles', to: 'articles#create'
+  get 'articles/:id', to: 'articles#show', as: :article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
