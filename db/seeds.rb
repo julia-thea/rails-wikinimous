201e9515f5ebbba5cdae39f5756e9c6b48f5b0f1
Julia Thea <yuliya.boyadjan@gmail.com>
2020-02-25 11:18:57 -0600

    Added Article model. Defined index, show, and create routes. Created views for those routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Article.destroy_all
+
+10.times do
+    Article.create!(
+      title: Faker::Company.buzzword,
+      content: Faker::TvShows::SouthPark.quote)
+end
+
+puts 'Finished!'
